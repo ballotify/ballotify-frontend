7eac78221e4089b6235e986c94e99011582b038d
Vasyl Stanislavchuk <vasyl.stanislavchuk@gmail.com>
2016-02-09 11:09:10 +0100

    Fixed FB initialization.

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,27 +7,60 @@ import Navigation from './Navigation';
 
 
 class App extends React.Component {
+    constructor (props, context) {
+        super(props, context);
+        this.state = {
+            fbLoaded: false
+        };
+    }
+
     componentDidMount() {
         window.fbAsyncInit = () => {
             FB.init({
-                appId       : '107790869310294',
-                xfbml       : true,
-                version     : 'v2.5'
+                appId   : '107790869310294',
+                status  : true,
+                xfbml   : true,
+                version : 'v2.5'
+            });
+
+            FB.getLoginStatus((response) => {
+                this.runFbInitCriticalCode();
             });
         };
+
+        (function (d, s, id) {
+            let js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        })(document, 'script', 'facebook-jssdk');
+    }
+
+    runFbInitCriticalCode() {
+        this.setState({'fbLoaded': true});
     }
 
     render() {
         const { auth, actions } = this.props;
 
-        return (
-            <div>
-                <Navigation auth={auth} actions={actions} />
-                <div className="app container">
-                    {this.props.children}
+        let app;
+
+        if (this.state.fbLoaded) {
+            app = (
+                <div>
+                    <Navigation auth={auth} actions={actions} />
+                    <div className="app container">
+                        {this.props.children}
+                    </div>
                 </div>
-            </div>
-        );
+            );
+        } else {
+            app = (<p>Loading</p>);
+        }
+
+        return app;
     }
 }
 
